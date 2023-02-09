f63f5013bf82636c21ba1f8cb902d687237bcd03
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2023-02-09 18:33:56 +0100

    wyświetlanie logów

--- app/src/main/java/wat/inz/kolektorlogow/MainActivity.java
+++ app/src/main/java/wat/inz/kolektorlogow/MainActivity.java
@@ -7,27 +7,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
-
-import wat.inz.kolektorlogow.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button refreshList;
     private List<Log> logList = new ArrayList<>();
     private TextView logListTextView;
-    private TextView helloView;
+    private ScrollView scrollViewLogs;
+    private TextView commandLine;
 
-    private ActivityMainBinding binding;
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +34,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         refreshList = findViewById(R.id.refreshList);
         logListTextView = findViewById(R.id.logList);
-        helloView=findViewById(R.id.textView);
         refreshList.setOnClickListener(this);
-
+        scrollViewLogs = findViewById(R.id.scrollViewList);
+commandLine=findViewById(R.id.commandLine);
 
     }
 
@@ -72,18 +71,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @SuppressLint("SetTextI18n")
     private void refreshLogList() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Log log : logList) {
-            stringBuilder.append(log.toString()).append("\n");
-        }
-
-        logListTextView.setText("Lista logów: \n\n" + stringBuilder);
+        logListTextView.setText("Lista logów: \n\n" + getAdbLogCat());
+        scrollViewLogs.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollViewLogs.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
     }
 
-    private void init() {
-        refreshList = findViewById(R.id.refreshList);
-        logListTextView = findViewById(R.id.logList);
-        helloView=findViewById(R.id.textView);
-        refreshList.setOnClickListener(this);
+    private String getAdbLogCat() {
+        try {
+            StringBuilder logBuilder = new StringBuilder();
+            String[] homeCommand = {"pwd"};
+            String[] homeCommand2 = {"ls", "-al"};
+            String[] pwdCommand = {"pwd"};
+            String[] logcatCommand = {"logcat", "-d"};
+            Process process = Runtime.getRuntime().exec(commandLine.getText().toString());
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            for (String log = br.readLine(); log != null; log = br.readLine()) {
+                logBuilder.append(log).append("\n\n");
+            }
+            br.close();
+            return logBuilder.toString();
+        } catch (IOException e) {
+            String err = "Cos jest nie tak" + e;
+            System.err.println(err);
+            return err;
+        }
     }
 }
