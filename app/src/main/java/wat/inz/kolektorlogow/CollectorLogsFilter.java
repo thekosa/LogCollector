8058fbf9ff84c8ff4841a91009bce79660fdb846
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2024-11-13 20:12:09 +0100

    filtrowanie jako srako

---
+++ app/src/main/java/wat/inz/kolektorlogow/CollectorLogsFilter.java
@@ -0,0 +1,46 @@
+package wat.inz.kolektorlogow;
+
+import java.util.Objects;
+
+import lombok.Data;
+
+public @Data class CollectorLogsFilter {
+    private String tagFilter;
+    private String priorityFilter;
+    private String pidFilter;
+    private String tidFilter;
+
+    public CollectorLogsFilter(String tagFilter, String priorityFilter, String pidFilter, String tidFilter) {
+        setFilter(tagFilter, priorityFilter, pidFilter, tidFilter);
+    }
+
+    public void setFilter(String tagFilter, String priorityFilter, String pidFilter, String tidFilter) {
+        if (isNull(tagFilter, priorityFilter, pidFilter, tidFilter)) {
+            clearFilters();
+            return;
+        }
+        this.tagFilter = normalizeFilter(tagFilter);
+        this.priorityFilter = normalizeFilter(priorityFilter);
+        this.pidFilter = normalizeFilter(pidFilter);
+        this.tidFilter = normalizeFilter(tidFilter);
+    }
+
+    private String normalizeFilter(String filter) {
+        return (filter == null || filter.isBlank() || filter.equals("*")) ? null : filter;
+    }
+
+    private void clearFilters() {
+        this.tagFilter = null;
+        this.priorityFilter = null;
+        this.pidFilter = null;
+        this.tidFilter = null;
+    }
+
+    public boolean isNull() {
+        return tagFilter == null && priorityFilter == null && pidFilter == null && tidFilter == null;
+    }
+
+    private boolean isNull(String tagFilter, String priorityFilter, String pidFilter, String tidFilter) {
+        return tagFilter == null && priorityFilter == null && pidFilter == null && tidFilter == null;
+    }
+}
--- app/src/main/java/wat/inz/kolektorlogow/MainActivity.java
+++ app/src/main/java/wat/inz/kolektorlogow/MainActivity.java
@@ -1,6 +1,7 @@
 package wat.inz.kolektorlogow;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -25,20 +27,28 @@ import com.google.android.material.navigation.NavigationView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     private Button refreshList;
     private TableLayout tableLayout;
     private String logcatCommand;
     private CollectorLogs collectorLogs;
     private DrawerLayout drawerLayout;
-    private ImageButton filterButton;
+    private ImageButton filtersLayoutButton;
     private NavigationView navigationView;
-    private Button saveButton;
+    private Button saveFiltersButton;
     private Spinner prioritySpinner;
+    private EditText tagInput;
+    private EditText pidInput;
+    private EditText tidInput;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch adbSwitch;
+    private CollectorLogsFilter logsListFilter;
+    private Map<String, String> priorityMap;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -46,20 +56,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-
         navigationView = findViewById(R.id.navigation_view);
-        saveButton = navigationView.findViewById(R.id.save_button);
+        saveFiltersButton = navigationView.findViewById(R.id.save_button);
         prioritySpinner = navigationView.findViewById(R.id.priority_spinner);
+        tagInput = navigationView.findViewById(R.id.tag_input);
+        pidInput = navigationView.findViewById(R.id.pid_input);
+        tidInput = navigationView.findViewById(R.id.tid_input);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-        filterButton = findViewById(R.id.filterButton);
+        filtersLayoutButton = findViewById(R.id.filterButton);
         refreshList = findViewById(R.id.refreshList);
         tableLayout = findViewById(R.id.tableLayout);
+        adbSwitch = findViewById(R.id.adb_switch);
 
         logcatCommand = "logcat -d";
         collectorLogs = new CollectorLogs();
+        logsListFilter = new CollectorLogsFilter(null, null, null, null);
 
-        saveButton.setOnClickListener(this);
+        saveFiltersButton.setOnClickListener(this);
+
+        priorityMap = new HashMap<>();
+        priorityMap.put("Verbose", "V");
+        priorityMap.put("Debug", "D");
+        priorityMap.put("Info", "I");
+        priorityMap.put("Warning", "W");
+        priorityMap.put("Error", "E");
+        priorityMap.put("Fatal", "F");
+        priorityMap.put("*", null);
     }
 
     @Override
@@ -87,33 +110,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == refreshList.getId()) {
+            logcatCommand = adbSwitch.isChecked() ? "adb logcat -d" : "logcat -d";
             refreshLogList();
-            for (CollectorLog log : collectorLogs.getLogsList()) {
-                TableRow row = new TableRow(this);
-                List<String> rowData = log.getRow();
-                for (String rowElement : rowData) {
-                    row.addView(createNewCellTextView(rowElement));
-                }
-                tableLayout.addView(row);
-            }
+            buildLogsListTableLayout();
         }
-        if (v.getId() == filterButton.getId()) {
+        if (v.getId() == filtersLayoutButton.getId()) {
             Toast.makeText(this, "Filter", Toast.LENGTH_SHORT).show();
             drawerLayout.openDrawer(GravityCompat.END);
         }
-        if (v.getId() == saveButton.getId()) {
-            Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show();
-            //todo: funkcjonalność switcha adb, w postaci dopisania adb do polecenia
-            //todo: funkcjonalność tag + priority, w postaci dopisania tagu i priorytetu do polecenia razem z *"S, chyba że nie ma jednego z nich to dopisać grepa
-            //todo: funkcjonalność pid i tid, w postaci wyfiltrowania listy logów po tych danych
+        if (v.getId() == saveFiltersButton.getId()) {
+            logsListFilter.setFilter(tagInput.getText().toString(),
+                    priorityMap.get(prioritySpinner.getSelectedItem().toString()),
+                    pidInput.getText().toString(),
+                    tidInput.getText().toString());
+            collectorLogs.filterOutLogs(logsListFilter);
+            buildLogsListTableLayout();
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
             drawerLayout.closeDrawer(navigationView);
         }
     }
-    //todo: sortownie
+    //todo: sortownie w zakładce z filtrami w postaci spinnera z nazwami kolumn i spinnera rosnąco/malejąco
+    //todo: przycisk filtrów wyszarzony/nieklikalny w momencie startu aplikacji, aż do momentu powstania listy logów
+    //todo: pozmieniać nazwy tej belki jako opcje wyświetlania, a nie filtrów
+    //todo: opcja wyświetlania bez jakichś kolumn w belce filtrów, jako grupa checkboxów
+    //todo: zrobić dwie listy logów, jedna pierwotna zmieniana tylko w momencie odświeżenia listy, druga filtrowana, zmieniana podłóg potrzeb
+    //todo: naprawić filtrowanie po wszystkich priorytetach
 
-    private TextView createNewCellTextView(String text) {
+    private void buildLogsListTableLayout() {
+        resetTableLayout();
+        for (CollectorLog log : collectorLogs.getLogsList()) {
+            TableRow row = new TableRow(this);
+            List<String> rowData = log.getRow();
+            for (String rowElement : rowData) {
+                row.addView(createNewCellTextView(rowElement, false));
+            }
+            tableLayout.addView(row);
+        }
+    }
+
+    private void resetTableLayout() {
+        tableLayout.removeAllViews();
+        TableRow row = new TableRow(this);
+        row.addView(createNewCellTextView("Date", true));
+        row.addView(createNewCellTextView("Time", true));
+        row.addView(createNewCellTextView("PID", true));
+        row.addView(createNewCellTextView("TID", true));
+        row.addView(createNewCellTextView("Priority", true));
+        row.addView(createNewCellTextView("Tag", true));
+        row.addView(createNewCellTextView("Message", true));
+        tableLayout.addView(row);
+
+    }
+
+    private void generateCommand(boolean adbSwitch, String tag, String priority) {
+        StringBuilder command = new StringBuilder();
+        if (adbSwitch) {
+            command.append("adb ");
+        }
+        command.append(logcatCommand);
+        boolean priorityIsNotUndefined = priority.compareTo("null") != 0 && priority.compareTo("*") != 0;
+        if (tag != null && priorityIsNotUndefined) {
+            command.append(tag).append(":").append(priority).append(" *:S");
+        } else if (tag != null && priority.compareTo("*") == 0) {
+            command.append(tag).append(":").append("V");
+        } else if (tag != null && priority.compareTo("null") == 0) {
+            command.append("|grep ").append(tag);
+        } else if (tag == null && priorityIsNotUndefined) {
+            command.append("|grep ").append(priority);
+        }
+        logcatCommand = command.toString();
+    }
+
+    private TextView createNewCellTextView(String text, boolean bold) {
         TextView cell = new TextView(this);
         cell.setText(text);
+        cell.setTypeface(null, bold ? Typeface.BOLD : Typeface.NORMAL);
         cell.setPadding(8, 8, 8, 8);
         return cell;
     }
@@ -123,7 +194,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Process process = Runtime.getRuntime().exec(logcatCommand);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             collectorLogs.destroyLogsList();
-            collectorLogs.generateLogs(bufferedReader);
+            collectorLogs.generateLogs(bufferedReader, logsListFilter);
             bufferedReader.close();
         } catch (IOException e) {
             String err = "Błąd polecenia logcat. ";
