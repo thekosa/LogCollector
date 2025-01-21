c75fa47dea1d2f251451ef05db1122a33a21d331
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-01-21 21:18:57 +0100

    dzialajacy test

---
+++ app/src/test/java/wat/inz/kolektorlogow/meta/FirestoreDeviceTest.java
@@ -0,0 +1,16 @@
+package wat.inz.kolektorlogow.meta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirestoreDeviceTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getIdentifier() {
+    }
+}
