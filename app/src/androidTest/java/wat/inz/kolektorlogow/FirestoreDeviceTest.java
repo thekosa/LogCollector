9eb6b896dce5d5dde387fe12e93d05f01f1039c3
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-01-23 13:47:55 +0100

    Updated Gradle and dependencies
    Updated Gradle to 8.10.2 and Android Gradle Plugin to 8.8.0.
    Removed ExampleUnitTest.java.
    Added a basic FirestoreDeviceTest class for Android testing.
    Updated dependencies for unit and Android testing.

---
+++ app/src/androidTest/java/wat/inz/kolektorlogow/FirestoreDeviceTest.java
@@ -0,0 +1,14 @@
+package wat.inz.kolektorlogow;
+
+import org.junit.Test;
+
+public class FirestoreDeviceTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getIdentifier() {
+    }
+}
